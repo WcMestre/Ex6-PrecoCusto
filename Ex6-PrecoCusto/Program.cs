6e392f679cb8273ab1c0f982a4b2ffb8e8ebddad
Wendel Côrtes Martins <wcmestre@hotmail.com>
2021-01-28 14:45:02 -0300

    Adicionar arquivos de projeto.

---
+++ Ex6-PrecoCusto/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex6_PrecoCusto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int custo, percentual, valorfinal;
+
+            Console.WriteLine("Digite o custo: ");
+            custo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o percentual: ");
+            percentual = int.Parse(Console.ReadLine());
+
+            valorfinal = (custo * percentual)/100;
+            valorfinal = valorfinal + custo;
+
+            Console.WriteLine("O valor final é: " + valorfinal);
+            Console.ReadKey();
+        }
+    }
+}
